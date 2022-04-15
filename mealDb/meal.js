9429ee056329506429ae17d50228830eab32b28a
farabitusher <61279854+farabitusher@users.noreply.github.com>
2022-04-15 21:19:10 +0600

    70% complete the project

--- mealDb/meal.js
+++ mealDb/meal.js
@@ -3,25 +3,37 @@ const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    searchField.value = '';
+  
     // const seachButton = document.getElementById('button-search')
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     console.log(url);
     //fetch the api
-    fetch(url)
-        .then(res => res.json())
-        .then(data =>displaySearchResult(data.meals))
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.meals));
+  //clear data
+  searchField.value = '';
+  if (data == 0) {
+    const result = document.getElementById('no-result');
+    result.style.display = 'inline';
+  }
+  
 }
 
 
 const displaySearchResult = meals => {
-    const searchResult = document.getElementById('search-result');
+  const searchResult = document.getElementById('search-result');
+  searchResult.textContent = '';
+  //delete click item after new search
+  const mealDetails = document.getElementById('meal-details');
+  mealDetails.textContent= ''
+ 
     meals.forEach(meal => {
         console.log(meal);
         const div = document.createElement('div');
-        div.classList.add('col');
+        // div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onClick="loadMealDetail(${meal.idMeal})" class="card col">
           <img src="${meal.strMealThumb}" class="card-img-top  img-fluid img-thumbnail" alt="..." />
           <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
@@ -32,4 +44,31 @@ const displaySearchResult = meals => {
         </div>`;
         searchResult.appendChild(div)
     });
+}
+
+// find the meal id 
+const loadMealDetail = mealId => {
+  console.log(mealId);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]));
+}
+const displayMealDetail = meal => {
+  console.log(meal);
+  const mealDetails = document.getElementById('meal-details');
+  mealDetails.textContent= ''
+  const div = document.createElement('div')
+
+  div.innerHTML = ` <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">
+    ${meal.strInstructions.slice(0,100)}
+    </p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Learn More</a>
+    
+  </div>`;
+  mealDetails.appendChild(div);
+
 }
